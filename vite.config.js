a8940723c3ca50bcf0c9841e65bf80c2c1882068
DhruvHedaoo2704 <dhruv.hedaoo@gmail.com>
2026-05-13 18:27:50 +0530

    Run deploy file

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/MessMind-AI/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
